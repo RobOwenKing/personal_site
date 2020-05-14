f95edc319863ff44b7bdbea4affe5a1b839bb8a9
Robert King <robert.o.king@hotmail.co.uk>
2020-05-14 15:01:27 +0200

    Tetris: mod function for screen wrap; buildGrid(); newPiece()

--- app/javascript/packs/projects/tetris.js
+++ app/javascript/packs/projects/tetris.js
@@ -9,25 +9,72 @@ let delay = 1000;
 let lastIteration = 0;
 let deltaTime = 0;
 
+const grid = [];
+
+const buildGrid = () => {
+  const row = [];
+  for (let i = 0; i < canvas.width / res; i += 1) {
+    row.push('.');
+  }
+  for (let j = 0; j < canvas.height / res; j += 1) {
+    grid.push(row);
+  }
+  console.log(grid);
+};
+
+buildGrid();
+
+const mod = (num) => {
+  limit = canvas.width / res
+  if (num < 0) {
+    num = limit + num;
+  } else if (num > (limit - 1)) {
+    num = num - limit;
+  }
+  return num;
+};
+
 const player = {
   x: 5,
   y: 0
 };
 
-const tPiece =  [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0]
-];
+const tPiece = {
+  piece: [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+  ],
+  colour: 'red',
+  yMargin: 1,
+  xOffset: 1
+};
 
-player.piece = tPiece;
+const oPiece = {
+  piece: [
+    [1, 1],
+    [1, 1]
+  ],
+  colour: 'blue',
+  yMargin: 0,
+  xOffset: 0
+};
+
+const pieces = [tPiece, oPiece];
+
+const newPiece = () => {
+  const newPiece = pieces[Math.floor(Math.random() * pieces.length)]
+  player.piece = newPiece;
+  player.x = 4 - newPiece.xOffset;
+  player.y = 0 - newPiece.yMargin;
+};
 
 const drawPiece = (piece, x, y) => {
-  for (let j = 0; j < piece.length; j += 1) {
-    for (let i = 0; i < piece[j].length; i += 1) {
-      if (piece[j][i] !== 0) {
-        ctx.fillStyle = 'red';
-        ctx.fillRect((i + x) * res,
+  for (let j = 0; j < piece.piece.length; j += 1) {
+    for (let i = 0; i < piece.piece[j].length; i += 1) {
+      if (piece.piece[j][i] !== 0) {
+        ctx.fillStyle = piece.colour;
+        ctx.fillRect(mod(i + x) * res,
                      (j + y) * res,
                      res, res);
       }
@@ -53,13 +100,19 @@ const update = (time = 0) => {
   requestAnimationFrame(update);
 };
 
+newPiece();
 update();
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'a') {
-    player.x = (player.x - 1) % 10;
+    player.x -= 1;
   }
   if (event.key === 'd') {
     player.x += 1;
   }
+  player.x = mod(player.x);
 });
+
+// Movement code
+// Piece array of objects
+// Piece centre offsets
