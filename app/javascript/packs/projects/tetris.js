c709e68d978c4e1636c508fa0492f9d9531c72f7
Robert King <robert.o.king@hotmail.co.uk>
2020-05-15 12:27:16 +0200

    Light and dark mode; pieceCollision()

--- app/javascript/packs/projects/tetris.js
+++ app/javascript/packs/projects/tetris.js
@@ -9,6 +9,10 @@ let delay = 1000;
 let lastIteration = 0;
 let deltaTime = 0;
 
+const dark = document.getElementById('dark');
+const light = document.getElementById('light');
+let mode = "dark";
+
 const grid = [];
 
 const buildGrid = () => {
@@ -41,8 +45,8 @@ const player = {
 const tPiece = {
   piece: [
     ['.', '.', '.'],
-    [1, 1, 1],
-    ['.', 1, '.']
+    [0, 0, 0],
+    ['.', 0, '.']
   ],
   colour: 'red',
   yMargin: 1,
@@ -51,8 +55,8 @@ const tPiece = {
 
 const oPiece = {
   piece: [
-    [1, 1],
-    [1, 1]
+    [30, 30],
+    [30, 30]
   ],
   colour: 'blue',
   yMargin: 0,
@@ -72,7 +76,7 @@ const drawPiece = (piece, x, y) => {
   for (let j = 0; j < piece.piece.length; j += 1) {
     for (let i = 0; i < piece.piece[j].length; i += 1) {
       if (piece.piece[j][i] !== '.') {
-        ctx.fillStyle = piece.colour;
+        ctx.fillStyle = mode === "dark" ? `hsl(${piece.colour},100%,50%)` : `hsl(${piece.colour},75%,75%)`;
         ctx.fillRect(mod(i + x) * unit,
                      (j + y) * unit,
                      unit, unit);
@@ -85,9 +89,9 @@ const draw = () => {
   for (let j = 0; j < grid.length; j += 1) {
     for (let i = 0; i < grid[j].length; i += 1) {
       if (grid[j][i] === '.') {
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = mode === "dark" ? "black" : "white";
       } else {
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = mode === "dark" ? `hsl(${grid[j][i]},100%,50%)` : `hsl(${grid[j][i]},75%,75%)`;
       }
       ctx.fillRect(i * unit, j * unit, unit, unit);
     }
@@ -96,16 +100,37 @@ const draw = () => {
   drawPiece(player.piece, player.x, player.y);
 };
 
+const pieceAtBottom = () => {
+  return (player.y + player.piece.piece.length) === (canvas.height / unit);
+};
+
+const pieceCollision = () => {
+  const piece = player.piece.piece;
+  for (let j = 0; j < piece.length; j += 1) {
+    for (let i = 0; i < piece[j].length; i += 1) {
+      if (piece[j][i] !== '.') {
+        if (grid[(player.y + j + 1)][mod(player.x + i)] !== '.') {
+          // console.log(grid[(player.y + j + 1)][mod(player.x + i)]);
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
+
 const addPieceToGrid = (piece, x, y) => {
   for (let j = 0; j < piece.length; j += 1) {
     for (let i = 0; i < piece[j].length; i += 1) {
-      grid[(y + j)][mod(x + i)] = piece[j][i];
+      if (piece[j][i] !== '.') {
+        grid[(y + j)][mod(x + i)] = piece[j][i];
+      }
     }
   }
 };
 
 const pieceLanding = () => {
-  if ((player.y + player.piece.piece.length) === (canvas.height / unit)) {
+  if (pieceAtBottom() || pieceCollision()) {
     addPieceToGrid(player.piece.piece, player.x, player.y);
     newPiece();
   }
@@ -134,4 +159,22 @@ document.addEventListener('keydown', (event) => {
     player.x += 1;
   }
   player.x = mod(player.x);
+});
+
+light.addEventListener('click', (event) => {
+  if (mode === "dark") {
+    mode = "light";
+    dark.classList.remove('btn-active');
+    light.classList.add('btn-active');
+    draw(grid);
+  }
+});
+
+dark.addEventListener('click', (event) => {
+  if (mode === "light") {
+    mode = "dark";
+    dark.classList.add('btn-active');
+    light.classList.remove('btn-active');
+    draw(grid);
+  }
 });
