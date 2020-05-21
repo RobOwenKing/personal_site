7130db8db766ebe6d3cf11e877c5959413b50b99
Robert King <robert.o.king@hotmail.co.uk>
2020-05-21 18:05:54 +0200

    Tetris highscores page and form

--- app/controllers/tetris_scores_controller.rb
+++ app/controllers/tetris_scores_controller.rb
@@ -1,15 +1,15 @@
 class TetrisScoresController < ApplicationController
   def index
-    @scores = TetrisScore.all
-  end
-
-  def new
-    @tetris_score = TetrisScore.new
+    @scores = TetrisScore.order(score: :desc).limit(25)
+    @scores_today = TetrisScore.order(score: :desc)
+                               .limit(25)
+                               .where(updated_at: (Time.now - 24.hours)..Time.now)
   end
 
   def create
     @tetris_score = TetrisScore.new(score_params)
     @tetris_score.save
+    redirect_to tetris_path
   end
 
   private
