dc37b91c0812e511d4de85da7f9105f46428679e
Robert King <robert.o.king@hotmail.co.uk>
2020-05-21 13:59:03 +0200

    Started tetris_score form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,5 @@
 class ProjectsController < ApplicationController
+  def tetris
+    @tetris_score = TetrisScore.new
+  end
 end
--- app/controllers/tetris_scores_controller.rb
+++ app/controllers/tetris_scores_controller.rb
@@ -4,8 +4,17 @@ class TetrisScoresController < ApplicationController
   end
 
   def new
+    @tetris_score = TetrisScore.new
   end
 
   def create
+    @tetris_score = TetrisScore.new(score_params)
+    @tetris_score.save
+  end
+
+  private
+
+  def score_params
+    params.require(:tetris_score).permit(:name, :score)
   end
 end
