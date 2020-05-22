c2ffdeb43d6f44b5dd4b5a73384cc7e6ac102f54
Robert King <robert.o.king@hotmail.co.uk>
2020-05-22 21:14:18 +0200

    Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio'
 
+  get 'game-of-life', to: 'projects#game_of_life'
   get 'tetris', to: 'projects#tetris'
   resources :tetris_scores, only: [ :index, :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
