f17475a7ecbc89873f7fe4df008469eb91336c4a
Robert King <robert.o.king@hotmail.co.uk>
2020-05-24 17:57:44 +0200

    Imported roman_calculator

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio'
 
-  get 'game-of-life', to: 'projects#game_of_life'
+  get 'colour_translator', to: 'projects#colour_translator'
+  get 'euler_spiral', to: 'projects#euler_spiral'
+  get 'game_of_life', to: 'projects#game_of_life'
+  get 'roman_calculator', to: 'projects#roman_calculator'
   get 'tetris', to: 'projects#tetris'
   resources :tetris_scores, only: [ :index, :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
