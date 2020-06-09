693d2a5d0fefd5dec605407aadd54a14b8e24dc9
Robert King <robert.o.king@hotmail.co.uk>
2020-06-10 00:41:45 +0200

    Validators for Anti-King and X-Sudoku

--- app/javascript/packs/projects/sudoku_validators.js
+++ app/javascript/packs/projects/sudoku_validators.js
@@ -1,6 +1,7 @@
 import { solutionArray } from './sudoku.js';
 
 const xSudoku = document.getElementById('x-sudoku');
+const antiKing = document.getElementById('anti-king');
 
 // Check whether a number already appears in a row
 const validInRow = (num, row) => {
@@ -37,7 +38,8 @@ const validInBlock = (num, x, y) => {
   return valid;
 };
 
-const validXLeftToRight = (num, x, y) => {
+// Validators for X-Sudoku
+const validXLtoR = (num) => {
   let valid = true;
   for (let i = 0; i < 9; i += 1) {
     if (solutionArray[i][i] === num) {
@@ -45,7 +47,45 @@ const validXLeftToRight = (num, x, y) => {
     }
   }
   return valid;
-}
+};
+
+const validXRtoL = (num) => {
+  let valid = true;
+  for (let i = 0; i < 9; i += 1) {
+    if (solutionArray[i][8 - i] === num) {
+      valid = false;
+    }
+  }
+  return valid;
+};
+
+// Validators for Chess Sudoku
+const validAntiKing = (num, x, y) => {
+  let valid = true;
+  // if (((x === 2 || x === 5) && (y > 0)) || ((x < 8) && (y === 3 || y === 6)))
+  // Below easier to write + maintain, plus the above needs a lot more comparisons
+  if ((y > 0) && (x < 8)) {
+    if (num === solutionArray[y - 1][x + 1]) {
+      valid = false;
+    }
+  }
+  if ((y > 0) && (x > 0)) {
+    if (num === solutionArray[y - 1][x - 1]) {
+      valid = false;
+    }
+  }
+  if ((y < 8) && (x < 8)) {
+    if (num === solutionArray[y + 1][x + 1]) {
+      valid = false;
+    }
+  }
+  if ((y < 8) && (x > 0)) {
+    if (num === solutionArray[y + 1][x - 1]) {
+      valid = false;
+    }
+  }
+  return valid;
+};
 
 // Checks whether a number can go in a cell (x,y) based on the others in its row, column and block
 const checkValid = (num, x, y) => {
@@ -54,6 +94,12 @@ const checkValid = (num, x, y) => {
   } else if (!validInCol(num, x)) {
     return false;
   } else if (!validInBlock(num, x, y)) {
+    return false;
+  } else if ((xSudoku.checked) && (x === y) && (!validXLtoR(num))) {
+    return false;
+  } else if ((xSudoku.checked) && (x === 8 - y) && (!validXRtoL(num))) {
+    return false;
+  } else if ((antiKing.checked) && (!validAntiKing(num, x, y))) {
     return false;
   } else {
     return true;
