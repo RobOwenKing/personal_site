f6afd72ce90d0b478c478f034e0d3a247ced24ee
Robert King <robert.o.king@hotmail.co.uk>
2020-06-14 00:09:20 +0200

    Started page for Quedem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio'
 
+  get 'quedem', to: 'projects#quedem'
+
   get 'colour_translator', to: 'projects#colour_translator'
   get 'euler_spiral', to: 'projects#euler_spiral'
   get 'game_of_life', to: 'projects#game_of_life'
@@ -9,8 +11,9 @@ Rails.application.routes.draw do
   get 'roman_calculator', to: 'projects#roman_calculator'
   get 'snake', to: 'projects#snake'
   get 'sudoku', to: 'projects#sudoku'
-  get 'tetris', to: 'projects#tetris'
   get 'wahnu', to: 'projects#wahnu'
+
+  get 'tetris', to: 'projects#tetris'
   resources :tetris_scores, only: [ :index, :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
