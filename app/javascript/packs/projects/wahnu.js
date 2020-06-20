e331524f32c1c85d76cd9e1036b2072af7ba2e06
Robert King <robert.o.king@hotmail.co.uk>
2020-06-20 19:35:30 +0200

    Edited Wahnu for basic function

--- app/javascript/packs/projects/wahnu.js
+++ app/javascript/packs/projects/wahnu.js
@@ -1,50 +1,55 @@
 const board = document.getElementById('board');
-
-const dimensions = [5, 4];
-const cols = [1, 3, 1, 2, 2];
-const rows = [1, 2, 1, 3];
+const puzzle = [
+  [ 3 ,'.','.','.','.'],
+  ['.','.','.','.','.'],
+  ['.','.','.','.', 3 ],
+  ['.','.','.','.','.'],
+  ['.','.','.','.','.']
+];
 const boardState = [];
 
-/*const dimensions = [4, 4];
-const cols = [1, 3, 1, 2];
-const rows = [1, 2, 1, 4];*/
-
 const fillBoard = () => {
-  // Create top row
-  board.insertAdjacentHTML('beforeend', '<tr><th></th>');
-  const headers = document.querySelector('#board > tr:last-child');
-  for (let i = 0; i < dimensions[0]; i += 1) {
-    headers.insertAdjacentHTML('beforeend', `<th>${cols[i]}</th>`);
-  }
-  board.insertAdjacentHTML('beforeend', '</tr>');
-
-  for (let j = 0; j < dimensions[1]; j += 1) {
-    board.insertAdjacentHTML('beforeend', `<tr><th>${rows[j]}</th>`);
+  for (let j = 0; j < puzzle.length; j += 1) {
+    board.insertAdjacentHTML('beforeend', `<tr>`);
     const latestRow = document.querySelector('#board > tr:last-child');
-    for (let k = 0; k < dimensions[0]; k += 1) {
-      latestRow.insertAdjacentHTML('beforeend', `<td data-x="${k}" data-y="${j}"></td>`);
+    for (let i = 0; i < puzzle[0].length; i += 1) {
+      if (puzzle[j][i] === '.') {
+        latestRow.insertAdjacentHTML('beforeend', `<td data-x="${i}" data-y="${j}" class="white clickable"></td>`);
+      } else {
+        latestRow.insertAdjacentHTML('beforeend', `<td data-x="${i}" data-y="${j}" class="white">${puzzle[j][i]}</td>`);
+      }
     }
     board.insertAdjacentHTML('beforeend', '</tr>');
   }
 };
 
 const createBoardState = () => {
-  for (let i = 0; i < dimensions[1]; i += 1) {
+  for (let j = 0; j < puzzle.length; j += 1) {
     const latestRow = [];
-    for (let j = 0; j < dimensions[0]; j += 1) {
-      latestRow.push('1');
+    for (let i = 0; i < puzzle[0].length; i += 1) {
+      latestRow.push(0);
     }
     boardState.push(latestRow);
   }
-  console.log(boardState);
 };
 
 const activateBoard = () => {
   const cells = document.querySelectorAll('#board td');
   cells.forEach((cell) => {
     cell.addEventListener('click', (event) => {
-      boardState[cell.dataset.y][cell.dataset.x] = 1 - boardState[cell.dataset.y][cell.dataset.x];
-      cell.style.opacity = boardState[cell.dataset.y][cell.dataset.x];
+      const j = cell.dataset.y;
+      const i = cell.dataset.x;
+
+      if (puzzle[j][i] === '.') {
+        if (boardState[j][i] === 0) {
+          cell.classList.remove('white');
+          cell.classList.add('black');
+        } else {
+          cell.classList.remove('black');
+          cell.classList.add('white');
+        }
+        boardState[j][i] = 1 - boardState[j][i];
+      }
     })
   })
 };
