41476efd24a70bcf45ba769b43de395e41676433
Robert King <robert.o.king@hotmail.co.uk>
2020-07-06 10:51:07 +0200

    Created User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_083108) do
+ActiveRecord::Schema.define(version: 2020_06_28_164906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_05_21_083108) do
   create_table "tetris_scores", force: :cascade do |t|
     t.string "name"
     t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
