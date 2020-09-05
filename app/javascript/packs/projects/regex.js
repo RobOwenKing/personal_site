480923e3cce5acd808e1088b32a168a883a2bbe6
Robert King <robert.o.king@hotmail.co.uk>
2020-09-05 19:09:04 +0200

    Check/cross marks for Regex challenge strings

--- app/javascript/packs/projects/regex.js
+++ app/javascript/packs/projects/regex.js
@@ -11,17 +11,61 @@ const testStrings = [
   ]
 ];
 
+const matches = [
+  ["Test"],
+  ["Tes"]
+];
+
 const reReplace = (string, i, re, valency) => {
+  const colour = valency === 'yes' ? 'green' : 'red';
   document.getElementById(`${valency}-${i}`)
-    .innerHTML = string.replace(re, '<span class="green-txt">$&</span>');
+    .innerHTML = string.replace(re, `<span class="${colour}-txt">$&</span>`);
 };
+
+const timesToCheck = icon => {
+  icon.classList.remove('fa-times-circle');
+  icon.classList.remove('red-txt');
+  icon.classList.add('fa-check-circle');
+  icon.classList.add('green-txt');
+};
+
+const checkToTimes = icon => {
+  icon.classList.remove('fa-check-circle');
+  icon.classList.remove('green-txt');
+  icon.classList.add('fa-times-circle');
+  icon.classList.add('red-txt');
+};
+
+const checkMatch = (i, match) => {
+  if (match === null) { return false; }
+
+  let bool = true;
+
+  matches[i].forEach((string, j) => {
+    if (string != match[j]) {
+      bool = false;
+    }
+  })
+  return bool;
+}
 
 const reMatch = (string, i, re, valency) => {
+  const match = string.match(re);
+  const icon = document.getElementById(`check-${valency}-${i}`);
 
-};
-
-const timesToCheck = (i, valency) => {
-
+  if (valency === 'no') {
+    if (match === null && icon.classList.contains('fa-times-circle')) {
+      timesToCheck(icon);
+    } else if (match !== null && icon.classList.contains('fa-check-circle')) {
+      checkToTimes(icon);
+    }
+  } else {
+    if (checkMatch(i, match) && icon.classList.contains('fa-times-circle')) {
+      timesToCheck(icon);
+    } else if (!checkMatch(i, match) && icon.classList.contains('fa-check-circle')) {
+      checkToTimes(icon);
+    }
+  }
 };
 
 regexInput.addEventListener('input', (event) => {
@@ -31,12 +75,14 @@ regexInput.addEventListener('input', (event) => {
     // document.getElementById(`yes-${i}`)
     //  .innerHTML = string.replace(re, '<span class="green-txt">$&</span>');
     reReplace(string, i, re, 'yes');
+    reMatch(string, i, re, 'yes');
   });
 
   testStrings[1].forEach((string, i) => {
     // document.getElementById(`no-${i}`)
     //  .innerHTML = string.replace(re, '<span class="red-txt">$&</span>');
     reReplace(string, i, re, 'no');
+    reMatch(string, i, re, 'no');
   });
 })
 
