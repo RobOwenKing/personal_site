a0249bc074fcaebd5c51251fc81bda45e9b9259c
Robert King <robert.o.king@hotmail.co.uk>
2020-09-18 13:53:32 +0200

    Linked star battle show JS to edit borders file

--- app/javascript/packs/puzzles/star_battle_show.js
+++ app/javascript/packs/puzzles/star_battle_show.js
@@ -1,5 +1,6 @@
 import { deepCopy } from '../shared/copy.js';
 import { Vars } from './star_battle/shared_vars.js';
+import { editCellBorders } from './star_battle/edit_borders.js';
 
 Vars.cagesBoard = JSON.parse(JSON.parse(Vars.board.parentElement.dataset.board));
 const sizeValue = Vars.cagesBoard.length;
@@ -73,61 +74,6 @@ const createCageBorders = () => {
 
     editCellBorders(cell, i, j);
   })
-};
-
-const editBorderTop = (cell, i, j) => {
-  if (Vars.cagesBoard[j - 1]) {
-    if (Vars.cagesBoard[j - 1][i] !== Vars.cagesBoard[j][i]) {
-      cell.classList.add('border-top');
-      Vars.board.childNodes[j - 1].childNodes[i].classList.add('border-bottom');
-    } else {
-      cell.classList.remove('border-top');
-      Vars.board.childNodes[j - 1].childNodes[i].classList.remove('border-bottom');
-    }
-  }
-};
-
-const editBorderBottom = (cell, i, j) => {
-  if (Vars.cagesBoard[j + 1]) {
-    if (Vars.cagesBoard[j + 1][i] !== Vars.cagesBoard[j][i]) {
-      cell.classList.add('border-bottom');
-      Vars.board.childNodes[j + 1].childNodes[i].classList.add('border-top');
-    } else {
-      cell.classList.remove('border-bottom');
-      Vars.board.childNodes[j + 1].childNodes[i].classList.remove('border-top');
-    }
-  }
-};
-
-const editBorderLeft = (cell, i, j) => {
-  if (i > 0) {
-    if (Vars.cagesBoard[j][i - 1] !== Vars.cagesBoard[j][i]) {
-      cell.classList.add('border-left');
-      Vars.board.childNodes[j].childNodes[i - 1].classList.add('border-right');
-    } else {
-      cell.classList.remove('border-left');
-      Vars.board.childNodes[j].childNodes[i - 1].classList.remove('border-right');
-    }
-  }
-};
-
-const editBorderRight = (cell, i, j) => {
-  if (i < Vars.cagesBoard[0].length - 1) {
-    if (Vars.cagesBoard[j][i + 1] !== Vars.cagesBoard[j][i]) {
-      cell.classList.add('border-right');
-      Vars.board.childNodes[j].childNodes[i + 1].classList.add('border-left');
-    } else {
-      cell.classList.remove('border-right');
-      Vars.board.childNodes[j].childNodes[i + 1].classList.remove('border-left');
-    }
-  }
-};
-
-const editCellBorders = (cell, i ,j) => {
-  editBorderTop(cell, i, j);
-  editBorderBottom(cell, i, j);
-  editBorderLeft(cell, i, j);
-  editBorderRight(cell, i, j);
 };
 
 const activateBoard = () => {
