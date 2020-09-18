6df88e0ed2d6500fc8047cab5bde8b2e738cc751
Robert King <robert.o.king@hotmail.co.uk>
2020-09-18 15:40:37 +0200

    Linked star battle show JS to clicks file

--- app/javascript/packs/puzzles/star_battle_show.js
+++ app/javascript/packs/puzzles/star_battle_show.js
@@ -1,6 +1,7 @@
 import { VARS, createAnswerBoard } from './star_battle/setup.js';
 import { editCellBorders } from './star_battle/edit_borders.js';
 import { initClearStars } from './star_battle/clear.js';
+import { solveModeClick } from './star_battle/clicks.js';
 
 VARS.cagesBoard = JSON.parse(JSON.parse(VARS.board.parentElement.dataset.board));
 VARS.sizeValue = VARS.cagesBoard.length;
@@ -21,28 +22,6 @@ const fillBoard = () => {
       latestRow.insertAdjacentHTML('beforeend', `<td data-x="${i}" data-y="${j}" class="white clickable" draggable="false"></td>`);
     }
     VARS.board.insertAdjacentHTML('beforeend', '</tr>');
-  }
-};
-
-const solveModeClick = (cell, i, j) => {
-  VARS.answerBoard[j][i] = (VARS.answerBoard[j][i] + 1) % 4;
-
-  if (VARS.answerBoard[j][i] === 0) {
-    cell.classList.remove('blue-txt');
-    cell.classList.add('white');
-    cell.innerHTML = '';
-  } else if (VARS.answerBoard[j][i] === 1) {
-    cell.classList.remove('white');
-    cell.classList.add('red-txt');
-    cell.innerHTML = '<i class="fas fa-times"></i>';
-  } else if (VARS.answerBoard[j][i] === 2) {
-    cell.classList.remove('red-txt');
-    cell.classList.add('yellow-txt');
-    cell.innerHTML = '<i class="fas fa-star"></i>';
-  } else {
-    cell.classList.remove('yellow-txt');
-    cell.classList.add('blue-txt');
-    cell.innerHTML = '<i class="fas fa-star-half"></i>';
   }
 };
 
