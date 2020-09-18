77131a397222933cccc56aa0b9297c8b79579b14
Robert King <robert.o.king@hotmail.co.uk>
2020-09-18 12:44:09 +0200

    Star Battle Show JS now uses Shared Vars

--- app/javascript/packs/puzzles/star_battle_show.js
+++ app/javascript/packs/puzzles/star_battle_show.js
@@ -1,25 +1,23 @@
 import { deepCopy } from '../shared/copy.js';
+import { Vars } from './star_battle/shared_vars.js';
 
-const board = document.getElementById('board');
-let cells;
-const cagesBoard = JSON.parse(JSON.parse(board.parentElement.dataset.board));
-const sizeValue = cagesBoard.length;
-const answerBoard = [];
+Vars.cagesBoard = JSON.parse(JSON.parse(Vars.board.parentElement.dataset.board));
+const sizeValue = Vars.cagesBoard.length;
 
 const checkGrid = document.getElementById('check-grid');
 const clearGrid = document.getElementById('clear-grid');
 
-const stars = parseInt(JSON.parse(board.parentElement.dataset.stars));
+const stars = parseInt(JSON.parse(Vars.board.parentElement.dataset.stars));
 
 const fillBoard = () => {
-  board.innerHTML = '';
+  Vars.board.innerHTML = '';
   for (let j = 0; j < sizeValue; j += 1) {
-    board.insertAdjacentHTML('beforeend', `<tr draggable="false">`);
+    Vars.board.insertAdjacentHTML('beforeend', `<tr draggable="false">`);
     const latestRow = document.querySelector('#board > tr:last-child');
     for (let i = 0; i < sizeValue; i += 1) {
       latestRow.insertAdjacentHTML('beforeend', `<td data-x="${i}" data-y="${j}" class="white clickable" draggable="false"></td>`);
     }
-    board.insertAdjacentHTML('beforeend', '</tr>');
+    Vars.board.insertAdjacentHTML('beforeend', '</tr>');
   }
 };
 
@@ -29,22 +27,22 @@ const createBoards = () => {
     for (let i = 0; i < sizeValue; i += 1) {
       latestRow.push(0);
     }
-    answerBoard.push(deepCopy(latestRow));
+    Vars.answerBoard.push(deepCopy(latestRow));
   }
 };
 
 const solveModeClick = (cell, i, j) => {
-  answerBoard[j][i] = (answerBoard[j][i] + 1) % 4;
+  Vars.answerBoard[j][i] = (Vars.answerBoard[j][i] + 1) % 4;
 
-  if (answerBoard[j][i] === 0) {
+  if (Vars.answerBoard[j][i] === 0) {
     cell.classList.remove('blue-txt');
     cell.classList.add('white');
     cell.innerHTML = '';
-  } else if (answerBoard[j][i] === 1) {
+  } else if (Vars.answerBoard[j][i] === 1) {
     cell.classList.remove('white');
     cell.classList.add('red-txt');
     cell.innerHTML = '<i class="fas fa-times"></i>';
-  } else if (answerBoard[j][i] === 2) {
+  } else if (Vars.answerBoard[j][i] === 2) {
     cell.classList.remove('red-txt');
     cell.classList.add('yellow-txt');
     cell.innerHTML = '<i class="fas fa-star"></i>';
@@ -59,17 +57,17 @@ const createTableBorders = () => {
   const height = sizeValue;
   const width = sizeValue;
   for (let i = 0; i < width; i += 1) {
-    board.childNodes[0].childNodes[i].classList.add('border-top');
-    board.childNodes[height - 1].childNodes[i].classList.add('border-bottom');
+    Vars.board.childNodes[0].childNodes[i].classList.add('border-top');
+    Vars.board.childNodes[height - 1].childNodes[i].classList.add('border-bottom');
   }
   for (let j = 0; j < height; j += 1) {
-    board.childNodes[j].childNodes[0].classList.add('border-left');
-    board.childNodes[j].childNodes[width - 1].classList.add('border-right');
+    Vars.board.childNodes[j].childNodes[0].classList.add('border-left');
+    Vars.board.childNodes[j].childNodes[width - 1].classList.add('border-right');
   }
 };
 
 const createCageBorders = () => {
-  cells.forEach((cell) => {
+  Vars.cells.forEach((cell) => {
     const j = parseInt(cell.dataset.y);
     const i = parseInt(cell.dataset.x);
 
@@ -78,49 +76,49 @@ const createCageBorders = () => {
 };
 
 const editBorderTop = (cell, i, j) => {
-  if (cagesBoard[j - 1]) {
-    if (cagesBoard[j - 1][i] !== cagesBoard[j][i]) {
+  if (Vars.cagesBoard[j - 1]) {
+    if (Vars.cagesBoard[j - 1][i] !== Vars.cagesBoard[j][i]) {
       cell.classList.add('border-top');
-      board.childNodes[j - 1].childNodes[i].classList.add('border-bottom');
+      Vars.board.childNodes[j - 1].childNodes[i].classList.add('border-bottom');
     } else {
       cell.classList.remove('border-top');
-      board.childNodes[j - 1].childNodes[i].classList.remove('border-bottom');
+      Vars.board.childNodes[j - 1].childNodes[i].classList.remove('border-bottom');
     }
   }
 };
 
 const editBorderBottom = (cell, i, j) => {
-  if (cagesBoard[j + 1]) {
-    if (cagesBoard[j + 1][i] !== cagesBoard[j][i]) {
+  if (Vars.cagesBoard[j + 1]) {
+    if (Vars.cagesBoard[j + 1][i] !== Vars.cagesBoard[j][i]) {
       cell.classList.add('border-bottom');
-      board.childNodes[j + 1].childNodes[i].classList.add('border-top');
+      Vars.board.childNodes[j + 1].childNodes[i].classList.add('border-top');
     } else {
       cell.classList.remove('border-bottom');
-      board.childNodes[j + 1].childNodes[i].classList.remove('border-top');
+      Vars.board.childNodes[j + 1].childNodes[i].classList.remove('border-top');
     }
   }
 };
 
 const editBorderLeft = (cell, i, j) => {
   if (i > 0) {
-    if (cagesBoard[j][i - 1] !== cagesBoard[j][i]) {
+    if (Vars.cagesBoard[j][i - 1] !== Vars.cagesBoard[j][i]) {
       cell.classList.add('border-left');
-      board.childNodes[j].childNodes[i - 1].classList.add('border-right');
+      Vars.board.childNodes[j].childNodes[i - 1].classList.add('border-right');
     } else {
       cell.classList.remove('border-left');
-      board.childNodes[j].childNodes[i - 1].classList.remove('border-right');
+      Vars.board.childNodes[j].childNodes[i - 1].classList.remove('border-right');
     }
   }
 };
 
 const editBorderRight = (cell, i, j) => {
-  if (i < cagesBoard[0].length - 1) {
-    if (cagesBoard[j][i + 1] !== cagesBoard[j][i]) {
+  if (i < Vars.cagesBoard[0].length - 1) {
+    if (Vars.cagesBoard[j][i + 1] !== Vars.cagesBoard[j][i]) {
       cell.classList.add('border-right');
-      board.childNodes[j].childNodes[i + 1].classList.add('border-left');
+      Vars.board.childNodes[j].childNodes[i + 1].classList.add('border-left');
     } else {
       cell.classList.remove('border-right');
-      board.childNodes[j].childNodes[i + 1].classList.remove('border-left');
+      Vars.board.childNodes[j].childNodes[i + 1].classList.remove('border-left');
     }
   }
 };
@@ -133,7 +131,7 @@ const editCellBorders = (cell, i ,j) => {
 };
 
 const activateBoard = () => {
-  cells.forEach((cell) => {
+  Vars.cells.forEach((cell) => {
     const j = parseInt(cell.dataset.y);
     const i = parseInt(cell.dataset.x);
 
@@ -147,7 +145,7 @@ const activateBoard = () => {
 const init = () => {
   fillBoard();
   createBoards();
-  cells = document.querySelectorAll('#board td');
+  Vars.cells = document.querySelectorAll('#board td');
   createTableBorders();
   createCageBorders();
   activateBoard();
@@ -158,8 +156,8 @@ init();
 clearGrid.addEventListener('click', (event) => {
   for (let j = 0; j < sizeValue; j += 1) {
     for (let i = 0; i < sizeValue; i += 1) {
-      const cell = board.childNodes[j].childNodes[i];
-      while (answerBoard[j][i] != 0) {
+      const cell = Vars.board.childNodes[j].childNodes[i];
+      while (Vars.answerBoard[j][i] != 0) {
         solveModeClick(cell, i, j);
       }
     }
@@ -169,7 +167,7 @@ clearGrid.addEventListener('click', (event) => {
 const countStarsInRow = (j) => {
   let count = 0;
   for (let i = 0; i < sizeValue; i += 1) {
-    if (answerBoard[j][i] == 2) {
+    if (Vars.answerBoard[j][i] == 2) {
       count += 1;
     }
   }
@@ -179,7 +177,7 @@ const countStarsInRow = (j) => {
 const countStarsInCol = (i) => {
   let count = 0;
   for (let j = 0; j < sizeValue; j += 1) {
-    if (answerBoard[j][i] == 2) {
+    if (Vars.answerBoard[j][i] == 2) {
       count += 1;
     }
   }
@@ -190,7 +188,7 @@ const countStarsInCage = (cage) => {
   let count = 0;
   for (let i = 0; i < sizeValue; i += 1) {
     for (let j = 0; j < sizeValue; j += 1) {
-      if (cagesBoard[j][i] == cage && answerBoard[j][i] == 2) {
+      if (Vars.cagesBoard[j][i] == cage && Vars.answerBoard[j][i] == 2) {
         count += 1;
       }
     }
@@ -200,11 +198,11 @@ const countStarsInCage = (cage) => {
 
 const checkNeighbourhoodEmpty = (i, j) => {
   if (j > 0) {
-    if (answerBoard[j-1][i-1] == 2) { return false; }
-    if (answerBoard[j-1][i] == 2) { return false; }
-    if (answerBoard[j-1][i+1] == 2) { return false; }
+    if (Vars.answerBoard[j-1][i-1] == 2) { return false; }
+    if (Vars.answerBoard[j-1][i] == 2) { return false; }
+    if (Vars.answerBoard[j-1][i+1] == 2) { return false; }
   }
-  if (answerBoard[j][i-1] == 2) { return false; }
+  if (Vars.answerBoard[j][i-1] == 2) { return false; }
 
   return true;
 };
@@ -219,10 +217,10 @@ checkGrid.addEventListener('click', (event) => {
   }
   for (let i = 0; i < sizeValue; i += 1) {
     for (let j = 0; j < sizeValue; j += 1) {
-      if (answerBoard[j][i] == 2 && !checkNeighbourhoodEmpty(i, j)) {
+      if (Vars.answerBoard[j][i] == 2 && !checkNeighbourhoodEmpty(i, j)) {
         correct = false;
       }
-      const currentCage = cagesBoard[j][i]
+      const currentCage = Vars.cagesBoard[j][i]
       if (checkedCages.indexOf(currentCage) == -1) {
         if (countStarsInCage(currentCage) != stars) {
           correct = false;
