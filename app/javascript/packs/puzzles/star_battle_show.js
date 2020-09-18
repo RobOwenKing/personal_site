dcdb2f22ab86375dc3a5be71d3babec40c8f18c8
Robert King <robert.o.king@hotmail.co.uk>
2020-09-18 16:13:30 +0200

    Split out Star Battle Show check grid code

--- app/javascript/packs/puzzles/star_battle_show.js
+++ app/javascript/packs/puzzles/star_battle_show.js
@@ -1,17 +1,14 @@
 import { VARS, createAnswerBoard } from './star_battle/setup.js';
 import { editCellBorders } from './star_battle/edit_borders.js';
 import { initClearStars } from './star_battle/clear.js';
+import { initCheckGrid } from './star_battle/check_grid.js';
 import { solveModeClick } from './star_battle/clicks.js';
 
 VARS.cagesBoard = JSON.parse(JSON.parse(VARS.board.parentElement.dataset.board));
 VARS.sizeValue = VARS.cagesBoard.length;
 
 initClearStars();
-
-const checkGrid = document.getElementById('check-grid');
-const clearGrid = document.getElementById('clear-grid');
-
-const stars = parseInt(JSON.parse(VARS.board.parentElement.dataset.stars));
+initCheckGrid();
 
 const fillBoard = () => {
   VARS.board.innerHTML = '';
@@ -54,7 +51,6 @@ const activateBoard = () => {
 
     cell.addEventListener('mousedown', (event) => {
       solveModeClick(cell, i, j);
-      countStarsInCol(i);
     })
   })
 };
@@ -69,72 +65,3 @@ const init = () => {
 };
 
 init();
-
-const countStarsInRow = (j) => {
-  let count = 0;
-  for (let i = 0; i < VARS.sizeValue; i += 1) {
-    if (VARS.answerBoard[j][i] == 2) {
-      count += 1;
-    }
-  }
-  return count;
-};
-
-const countStarsInCol = (i) => {
-  let count = 0;
-  for (let j = 0; j < VARS.sizeValue; j += 1) {
-    if (VARS.answerBoard[j][i] == 2) {
-      count += 1;
-    }
-  }
-  return count;
-};
-
-const countStarsInCage = (cage) => {
-  let count = 0;
-  for (let i = 0; i < VARS.sizeValue; i += 1) {
-    for (let j = 0; j < VARS.sizeValue; j += 1) {
-      if (VARS.cagesBoard[j][i] == cage && VARS.answerBoard[j][i] == 2) {
-        count += 1;
-      }
-    }
-  }
-  return count;
-};
-
-const checkNeighbourhoodEmpty = (i, j) => {
-  if (j > 0) {
-    if (VARS.answerBoard[j-1][i-1] == 2) { return false; }
-    if (VARS.answerBoard[j-1][i] == 2) { return false; }
-    if (VARS.answerBoard[j-1][i+1] == 2) { return false; }
-  }
-  if (VARS.answerBoard[j][i-1] == 2) { return false; }
-
-  return true;
-};
-
-checkGrid.addEventListener('click', (event) => {
-  let correct = true;
-  const checkedCages = [];
-  for (let i = 0; i < VARS.sizeValue; i += 1) {
-    if (countStarsInRow(i) != stars || countStarsInCol(i) != stars) {
-      correct = false;
-    }
-  }
-  for (let i = 0; i < VARS.sizeValue; i += 1) {
-    for (let j = 0; j < VARS.sizeValue; j += 1) {
-      if (VARS.answerBoard[j][i] == 2 && !checkNeighbourhoodEmpty(i, j)) {
-        correct = false;
-      }
-      const currentCage = VARS.cagesBoard[j][i]
-      if (checkedCages.indexOf(currentCage) == -1) {
-        if (countStarsInCage(currentCage) != stars) {
-          correct = false;
-        } else {
-          checkedCages.push(currentCage);
-        }
-      }
-    }
-  }
-  console.log(correct);
-})
