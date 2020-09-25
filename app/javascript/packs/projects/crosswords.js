423bf24b12533680b3a2adb920285860f50b5efa
Robert King <robert.o.king@hotmail.co.uk>
2020-09-25 10:54:12 +0200

    Comments

--- app/javascript/packs/projects/crosswords.js
+++ app/javascript/packs/projects/crosswords.js
@@ -10,13 +10,17 @@ const createGrid = () => {
     const latestRow = document.querySelector('tbody tr:last-child');
     for (let i = 0; i < first[0].length; i += 1) {
       if (first[j][i] == '.') {
+        // If the cell is empty, make it black
         latestRow.insertAdjacentHTML('beforeend', '<td class="black"></td>');
       } else {
+        // Else, the cell should be white
         if ((first[j-1] && first[j-1][i] == '.') || first[j][i-1] == '.') {
+          // If the cell has a black cell to the left or above, it needs a question number
           latestRow.insertAdjacentHTML('beforeend',
             `<td class="white"><span class="q-number">${qNumber}</span><span contenteditable="true"></span></td>`);
           qNumber += 1;
         } else {
+          // Else plain white cell
           latestRow.insertAdjacentHTML('beforeend',
             `<td class="white"><span contenteditable="true"></span></td>`);
         }
