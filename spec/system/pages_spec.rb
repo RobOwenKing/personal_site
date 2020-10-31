02b4757b7c66a72bd6e36573cb69aa5fbba4c90d
Robert King <robert.o.king@hotmail.co.uk>
2020-10-31 23:01:36 +0100

    Added: Basic system tests for Movida and Quedem pages

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe "Pages", type: :system do
       expect(page).to have_content('Rob Owen King')
     end
   end
+  describe 'Movida' do
+    before { visit '/movida' }
+    it 'should have a title' do
+      expect(page).to have_content('Movida-19')
+    end
+    it 'should have a link to the website' do
+      expect(page).to have_link(href: /movida-19.com/)
+    end
+  end
+  describe 'Quedem' do
+    before { visit '/quedem' }
+    it 'should have a title' do
+      expect(page).to have_content('Quedem')
+    end
+    it 'should have a link to the website' do
+      expect(page).to have_link(href: /goquedem.com/)
+    end
+  end
 end
