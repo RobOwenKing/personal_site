d78e2866de3bfcd5b44d23042118d4abae43ad74
Robert King <robert.o.king@hotmail.co.uk>
2020-11-10 21:57:36 +0100

    Added: Basics of Q and A system

--- app/javascript/packs/projects/map_quiz.js
+++ app/javascript/packs/projects/map_quiz.js
@@ -1,3 +1,27 @@
-const place = document.getElementById('place');
+let questions = [
+  { prompt: "Alabama", answers: ['montgomery'] },
+  { prompt: "Alaska", answers: ['juneau'] },
+  { prompt: "Arizona", answers: ['phoenix'] },
+  { prompt: "Arkansas", answers: ['little rock'] }
+];
 
-place.innerText = "Arizona";
+const category = document.getElementById('category');
+const prompt = document.getElementById('prompt');
+const answerInput = document.getElementById('answer');
+let answerArray;
+
+const newQuestion = () => {
+  questions = questions.filter((element) => answerArray != element.answers);
+  let qNumber = Math.floor(Math.random() * questions.length);
+  prompt.innerText = questions[qNumber].prompt;
+  answerArray = questions[qNumber].answers;
+}
+
+answerInput.addEventListener('input', (event) => {
+  if (answerArray.includes(answerInput.value.toLowerCase())) {
+    newQuestion();
+    answerInput.value = '';
+  }
+})
+
+newQuestion();
