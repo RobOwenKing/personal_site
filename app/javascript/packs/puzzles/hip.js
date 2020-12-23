781f2bfe940b8d4b3b4e53d937360c0b688b38a9
Robert King <robert.o.king@hotmail.co.uk>
2020-12-24 00:53:41 +0100

    Refactored: Import deepCopy() and deepCopyObject() instead of duplicating

--- app/javascript/packs/puzzles/hip.js
+++ app/javascript/packs/puzzles/hip.js
@@ -1,36 +1,4 @@
-const deepCopy = (arr) => {
-  let copy = [];
-  arr.forEach(elem => {
-    if(Array.isArray(elem)){
-      copy.push(deepCopy(elem))
-    }else{
-      if (typeof elem === 'object') {
-        copy.push(deepCopyObject(elem))
-    } else {
-        copy.push(elem)
-      }
-    }
-  })
-  return copy;
-}
-
-const deepCopyObject = (obj) => {
-  let tempObj = {};
-  for (let [key, value] of Object.entries(obj)) {
-    if (Array.isArray(value)) {
-      tempObj[key] = deepCopy(value);
-    } else {
-      if (typeof value === 'object') {
-        tempObj[key] = deepCopyObject(value);
-      } else {
-        tempObj[key] = value
-      }
-    }
-  }
-  return tempObj;
-}
-
-
+import { deepCopy, deepCopyObject } from '../shared/copy.js';
 
 const rows = document.getElementById('rows');
 const cols = document.getElementById('cols');
