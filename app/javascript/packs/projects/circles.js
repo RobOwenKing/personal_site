e2e9f20eb678fad75a05bc0f06a3b1a3f7e8c442
Robert King <robert.o.king@hotmail.co.uk>
2020-12-31 15:19:01 +0100

    Deleted: Old Circles JS; Added: Calculating canvas size

--- app/javascript/packs/projects/circles.js
+++ app/javascript/packs/projects/circles.js
@@ -1,27 +1,21 @@
-const canvas = document.getElementById('circles-canvas');
+const canvas = document.getElementById('circles');
 const ctx = canvas.getContext('2d');
 
-const page = 1;
+// Viewport size based on code from https://stackoverflow.com/questions/3437786
+const docElem = document.documentElement;
+const body = document.getElementsByTagName('body')[0];
+const viewWidth = window.innerWidth || docElem.clientWidth || body.clientWidth;
+const viewHeight = window.innerHeight|| docElem.clientHeight|| body.clientHeight;
 
+const width = 0.6 * viewWidth;
+const height = 0.6 * viewHeight;
+
+canvas.width = width;
+canvas.height = height;
 
 const draw = () => {
   ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  for (let x = 0; x < canvas.width; x += 10) {
-    for (let y = 0; y <= canvas.height; y += 10) {
-      ctx.beginPath();
-
-      const colour = ((x + 3) * (y - 3) * page) % 359;
-      ctx.strokeStyle = `hsl(${colour},75%,75%)`;
-
-      const radius = ((x + 11) * (y + 13) * page) % 150;
-
-      ctx.arc(x + 5, y + 5, 3 + (radius / 100), 0, Math.PI * 2, true);
-
-      ctx.stroke();
-    }
-  }
+  ctx.fillRect(0, 0, width, height);
 };
 
 draw();
