02f26ceaf1701f774a9b10878dffcc3eb678ce14
Robert King <robert.o.king@hotmail.co.uk>
2021-01-02 22:33:12 +0100

    Added: Basic conversion

--- app/javascript/packs/projects/money.js
+++ app/javascript/packs/projects/money.js
@@ -35,8 +35,12 @@ const selectOptions = `<option value="USD" selected="selected">United States Dol
 let rates;
 let ratesRetrieved;
 
-const inputValue = document.getElementById('input-value');
+const inputNumber   = document.getElementById('input-value');
 const inputCurrency = document.getElementById('input-currency');
+const outputCurrency = document.getElementById('output-currency');
+const outputCountry  = document.getElementById('output-country');
+const input  = document.getElementById('input');
+const output = document.getElementById('output');
 
 const getRates = () => {
   fetch('https://api.exchangeratesapi.io/latest')
@@ -48,40 +52,21 @@ const getRates = () => {
       });
 };
 
+const convert = () => {
+  const inputValue = inputNumber.value;
+  const firstRate  = rates[inputCurrency.value];
+  const secondRate = rates[outputCurrency.value];
+
+  const outputValue = (inputValue / firstRate) * secondRate;
+
+  input.innerHTML  = `${inputValue} ${inputCurrency.value}`;
+  output.innerHTML = `${outputValue} ${outputCurrency.value}`;
+};
+
+inputNumber.addEventListener('input', (event) => {
+  convert();
+})
+
 inputCurrency.insertAdjacentHTML('beforeend', selectOptions);
-//getRates();
-
-
-AUD: 1.5896
-BGN: 1.9558
-BRL: 6.3735
-CAD: 1.5633
-CHF: 1.0802
-CNY: 8.0225
-CZK: 26.242
-DKK: 7.4409
-EUR: 1
-GBP: 0.89903
-HKD: 9.5142
-HRK: 7.5519
-HUF: 363.89
-IDR: 17240.76
-ILS: 3.9447
-INR: 89.6605
-ISK: 156.1
-JPY: 126.49
-KRW: 1336
-MXN: 24.416
-MYR: 4.934
-NOK: 10.4703
-NZD: 1.6984
-PHP: 59.125
-PLN: 4.5597
-RON: 4.8683
-RUB: 91.4671
-SEK: 10.0343
-SGD: 1.6218
-THB: 36.727
-TRY: 9.1131
-USD: 1.2271
-ZAR: 18.0219
+outputCurrency.insertAdjacentHTML('beforeend', selectOptions);
+getRates();
