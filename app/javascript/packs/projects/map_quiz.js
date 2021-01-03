6cb821b2ff6ee48f8c6c5917fbd2211e68b56b87
Robert King <robert.o.king@hotmail.co.uk>
2021-01-03 21:13:20 +0100

    Added: All remaining US state capitals; Edited: System to pick questions

--- app/javascript/packs/projects/map_quiz.js
+++ app/javascript/packs/projects/map_quiz.js
@@ -1,8 +1,56 @@
+// https://hjnilsson.github.io/country-flags/
+
 let questions = [
   { prompt: "Alabama", answers: ['montgomery'] },
   { prompt: "Alaska", answers: ['juneau'] },
   { prompt: "Arizona", answers: ['phoenix'] },
-  { prompt: "Arkansas", answers: ['little rock'] }
+  { prompt: "Arkansas", answers: ['little rock'] },
+  { prompt: "California", answers: ['sacramento'] },
+  { prompt: "Colorado", answers: ['denver'] },
+  { prompt: "Connecticut", answers: ['hartford'] },
+  { prompt: "Delaware", answers: ['dover'] },
+  { prompt: "Florida", answers: ['tallahassee'] },
+  { prompt: "Georgia", answers: ['atlanta'] },
+  { prompt: "Hawaii", answers: ['honolulu'] },
+  { prompt: "Idaho", answers: ['boise'] },
+  { prompt: "Illinois", answers: ['springfield'] },
+  { prompt: "Indiana", answers: ['indianapolis'] },
+  { prompt: "Iowa", answers: ['des moines'] },
+  { prompt: "Kansas", answers: ['topeka'] },
+  { prompt: "Kentucky", answers: ['frankfort'] },
+  { prompt: "Louisiana", answers: ['baton rouge'] },
+  { prompt: "Maine", answers: ['augusta'] },
+  { prompt: "Maryland", answers: ['annapolis'] },
+  { prompt: "Massachusetts", answers: ['boston'] },
+  { prompt: "Michigan", answers: ['lansing'] },
+  { prompt: "Minnesota", answers: ['saint paul'] },
+  { prompt: "Mississippi", answers: ['jackson'] },
+  { prompt: "Missouri", answers: ['jefferson city'] },
+  { prompt: "Montana", answers: ['helena'] },
+  { prompt: "Nebraska", answers: ['lincoln'] },
+  { prompt: "Nevada", answers: ['carson city'] },
+  { prompt: "New Hampshire", answers: ['concord'] },
+  { prompt: "New Jersey", answers: ['trenton'] },
+  { prompt: "New Mexico", answers: ['santa fe'] },
+  { prompt: "New York", answers: ['albany'] },
+  { prompt: "North Carolina", answers: ['raleigh'] },
+  { prompt: "North Dakota", answers: ['bismarck'] },
+  { prompt: "Ohio", answers: ['columbus'] },
+  { prompt: "Oklahoma", answers: ['oklahoma city'] },
+  { prompt: "Oregon", answers: ['salem'] },
+  { prompt: "Pennsylvania", answers: ['harrisburg'] },
+  { prompt: "Rhode Island", answers: ['providence'] },
+  { prompt: "South Carolina", answers: ['columbia'] },
+  { prompt: "South Dakota", answers: ['pierre'] },
+  { prompt: "Tennessee", answers: ['nashville'] },
+  { prompt: "Texas", answers: ['austin'] },
+  { prompt: "Utah", answers: ['salt lake city'] },
+  { prompt: "Vermont", answers: ['montpelier'] },
+  { prompt: "Virginia", answers: ['richmond'] },
+  { prompt: "Washington", answers: ['olympia'] },
+  { prompt: "West Virginia", answers: ['charleston'] },
+  { prompt: "Wisconsin", answers: ['madison'] },
+  { prompt: "Wyoming", answers: ['cheyenne'] }
 ];
 
 const start = document.getElementById('start');
@@ -12,22 +60,33 @@ const category = document.getElementById('category');
 const prompt = document.getElementById('prompt');
 const answerInput = document.getElementById('answer');
 let answerArray;
+let score = 0;
 
 const newQuestion = () => {
-  questions = questions.filter((element) => answerArray != element.answers);
-  let qNumber = Math.floor(Math.random() * questions.length);
-  prompt.innerText = questions[qNumber].prompt;
-  answerArray = questions[qNumber].answers;
+  const question = questions.pop();
+  prompt.innerHTML = question.prompt;
+  answerArray = question.answers;
+};
+
+const gameOver = () => {
+
 };
 
 answerInput.addEventListener('input', (event) => {
   if (answerArray.includes(answerInput.value.toLowerCase())) {
-    newQuestion();
-    answerInput.value = '';
+    score += 1;
+    if (questions.length > 0) {
+      newQuestion();
+      answerInput.value = '';
+    } else {
+      gameOver();
+    }
   }
 });
 
 start.addEventListener('click', (event) => {
   game.style.display = "block";
   start.style.display = "none";
+  questions.sort((a, b) => Math.random() - 0.5);
+  newQuestion();
 });
