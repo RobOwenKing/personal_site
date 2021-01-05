0c76296322816806a55f5d5ac64f605b90ce6eea
Robert King <robert.o.king@hotmail.co.uk>
2021-01-05 22:49:12 +0100

    Added: stroke-width and fill change when mouseover a path

--- app/javascript/packs/projects/maps_quiz.js
+++ app/javascript/packs/projects/maps_quiz.js
@@ -6,14 +6,19 @@ const mapDisplay = document.getElementById("map");
 const init = () => {
   mapDisplay.innerHTML = svgUS;
   const paths = mapDisplay.querySelectorAll('path');
-  const names = [];
   paths.forEach((path) => {
     path.addEventListener('click', (event) => {
       console.log(path.dataset.name);
     })
-    names.push(path.dataset.name);
+    path.addEventListener('mouseover', (event) => {
+      path.style.strokeWidth = 3;
+      path.style.fill = '#C5DBF5';
+    })
+    path.addEventListener('mouseout', (event) => {
+      path.style.strokeWidth = 1;
+      path.style.fill = '#f9f9f9';
+    })
   });
-  console.log(names);
 };
 
 init();
