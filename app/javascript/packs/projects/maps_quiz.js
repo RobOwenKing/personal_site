3bd6d990a889c6d928576e233a2af9b3e15e335a
Robert King <robert.o.king@hotmail.co.uk>
2021-01-08 15:15:00 +0100

    Added: Time to results

--- app/javascript/packs/projects/maps_quiz.js
+++ app/javascript/packs/projects/maps_quiz.js
@@ -9,11 +9,13 @@ const playingUI = document.getElementById("playing-ui");
 const results = document.getElementById("results");
 const resultsScore = document.getElementById("results-score");
 const resultsTotal = document.getElementById("results-total");
+const timeDisplay = document.getElementById('time');
 const restart = document.getElementById("restart");
 
 let promptsArray, prompt;
 let score = 0;
 let filled = [];
+let startTime, endTime;
 
 const handlePathClick = (path) => {
   if (path.id == prompt) {
@@ -58,10 +60,21 @@ restart.addEventListener("click", (event) => {
   score = 0;
 })
 
+// Below based on code from https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
+const formatTime = (millis) => {
+  const mins = Math.floor(millis / 60000);
+  const secs = ((millis % 60000) / 1000).toFixed(0);
+  return mins + ":" + (secs < 10 ? '0' : '') + secs;
+};
+
 const gameOver = () => {
   resultsScore.innerHTML = score;
   playingUI.style.display = "none";
   results.style.display = "block";
+
+  endTime = new Date();
+  const timeInMilliseconds = endTime - startTime;
+  timeDisplay.innerHTML = formatTime(timeInMilliseconds);
 };
 
 const newQuestion = () => {
@@ -83,6 +96,7 @@ const init = () => {
   scoreDisplay.innerHTML = 0;
   totalDisplay.innerHTML = promptsArray.length;
   resultsTotal.innerHTML = promptsArray.length;
+  startTime = new Date();
   activatePaths();
   newQuestion();
 };
