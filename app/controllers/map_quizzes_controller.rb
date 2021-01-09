51ecbeaee9a7551635fa47b48edd6389f64338de
Robert King <robert.o.king@hotmail.co.uk>
2021-01-09 19:16:39 +0100

    Added: Basic Controller for MapQuizzes

--- app/controllers/map_quizzes_controller.rb
+++ app/controllers/map_quizzes_controller.rb
@@ -1,13 +1,33 @@
 class MapQuizzesController < ApplicationController
+  http_basic_authenticate_with name: User.first.name, password: User.first.password, only: :new
+
   def index
+    map_quizzes = MapQuiz.all
+    @internationals = map_quizzes.filter { |quiz| quiz.category == 'int' }
+    @nationals = map_quizzes.filter { |quiz| quiz.category == 'nat' }
   end
 
   def show
+    @map_quiz = MapQuiz.find(params[:id])
   end
 
   def new
+    @map_quiz = MapQuiz.new
   end
 
   def create
+    @map_quiz = MapQuiz.new(map_quiz_params)
+
+    if @map_quiz.save
+      redirect_to map_quiz_path(@map_quiz)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def star_battle_params
+    params.require(:map_quiz).permit(:name, :svg, :prompts, :category)
   end
 end
