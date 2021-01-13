2b9dca26c405d9bf00c97deaeabe7f8f723e5b68
Robert King <robert.o.king@hotmail.co.uk>
2021-01-13 16:41:45 +0100

    Edit: Second attempt at making drawRose start from 12 o'clock

--- app/javascript/packs/projects/clocks/rose.js
+++ app/javascript/packs/projects/clocks/rose.js
@@ -6,12 +6,14 @@ const ctxRose = canvasRose.getContext('2d');
 canvasRose.width = 240;
 canvasRose.height = 180;
 
+const halfPi = Math.PI * 0.5;
+
 const drawLoop = (min, max, step, k, radius) => {
   for (let i = min; i <= max; i += step) {
     const theta = (i / 180) * Math.PI;
     const r = 60 * Math.cos(k * theta);
-    const x = r * Math.cos(theta);
-    const y = r * Math.sin(theta);
+    const x = r * Math.cos(theta - halfPi);
+    const y = r * Math.sin(theta - halfPi);
 
     ctxRose.beginPath();
     ctxRose.fillStyle = `hsl(${i}, 100%, 50%)`;
@@ -30,14 +32,14 @@ export const drawRose = (state) => {
   // So stepMins = 360 / 60
   const stepMins = 6;
   // We need to subtract 90 to start from the vertical
-  const maxMins = (state.mins * stepMins) - 90;
+  const maxMins = state.mins * stepMins;
   // 60 secs in a minute, so stepSecs = stepMins / 60
   const stepSecs = 0.1;
   const maxSecs = maxMins + (state.secs * stepSecs);
 
   ctxRose.translate(canvasRose.width / 2, canvasRose.height / 2);
 
-  drawLoop(-90, maxMins, stepMins, k, 2);
+  drawLoop(0, maxMins, stepMins, k, 2);
   drawLoop(maxMins, maxSecs, stepSecs, k, 1);
 
   ctxRose.translate(-(canvasRose.width / 2), -(canvasRose.height / 2));
