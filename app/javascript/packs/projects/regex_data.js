8f53c09faf78fefcd8fe418a96a3d4bab8832eb3
Robert King <robert.o.king@hotmail.co.uk>
2021-01-14 12:59:04 +0100

    Added: Regex-ception solution

--- app/javascript/packs/projects/regex_data.js
+++ app/javascript/packs/projects/regex_data.js
@@ -111,8 +111,8 @@ const roman = {
     ["MDCCXXIX"]
   ],
   solution: /\bM{0,3}(C[MD]|D?C{0,3})?(X[CL]|L?X{0,3})?(I[XV]|V?I{0,3})?\b/g
-}
-/*
+};
+
 const regex = {
   name: "Regex-ception",
   value: "regex-ception",
@@ -133,8 +133,8 @@ const regex = {
     ["/colou?r/"],
     ["/\\d+/"]
   ],
-  solution: //
-};*/
+  solution: /\/[\(\\]?[\w\?]+[\)\+]?\//gi
+};
 
 const titles = {
   name: "I'm not great at titles",
@@ -148,7 +148,7 @@ const titles = {
       "Fr. Ted"
     ],
     [
-      "MRI",
+      "Ar.",
       "Mississippi",
       "Drs"
     ],
@@ -166,4 +166,6 @@ const snowclones = {};
 
 const comments = {};
 
-export const challenges = [helloWorld, ordinals, catalan, titles, roman];
+const attributes = {};
+
+export const challenges = [helloWorld, ordinals, catalan, titles, roman, regex];
