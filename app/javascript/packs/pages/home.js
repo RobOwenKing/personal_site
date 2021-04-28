49a5ee2201b268733194c03edfa5248cc3fe52b4
Robert King <robert.o.king@hotmail.co.uk>
2021-04-28 21:56:49 +0200

    Edited: Hallo to Servus on home page

--- app/javascript/packs/pages/home.js
+++ app/javascript/packs/pages/home.js
@@ -1,5 +1,5 @@
 const element = document.getElementById('greeting');
-const greetings = ['Hi!', 'Hola!', 'Salut!', 'Hallo!', 'Cześć!', '你好!'];
+const greetings = ['Hi!', 'Hola', 'Salut', 'Servus', 'Cześć', '你好'];
 let current = 0;
 
 window.setInterval(() => {
