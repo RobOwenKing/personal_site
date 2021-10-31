fd345e23c10643d3dbca07af63c2c9929adb97ad
Robert King <robert.o.king@hotmail.co.uk>
2021-10-31 23:31:22 +0000

    Added: Route for Hanzi patterns page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'movida', to: 'pages#movida'
   get 'quedem', to: 'pages#quedem'
 
+  get 'patterns_in_the_hanzi', to: 'pages#hanzi_patterns'
   get 'up_to_speed_korean', to: 'pages#react_korean'
 
   get 'capitals_quiz', to: 'projects#capitals_quiz'
