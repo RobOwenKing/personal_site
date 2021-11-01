37c398e4ee48be3932b441ae869067a0877c2417
Robert King <robert.o.king@hotmail.co.uk>
2021-11-01 16:34:48 +0000

    Refactored: _navbar tests moved to separate shared_spec

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
-  describe '_navbar' do
-    before { visit game_of_life_path }
-    it 'should link to home page' do
-      click_on 'Home'
-      expect(page).to have_current_path(root_path)
-    end
-    it 'should link to portfolio' do
-      click_on 'Portfolio'
-      expect(page).to have_current_path(portfolio_path)
-    end
-  end
   describe 'Colour Translator' do
     before { visit colour_translator_path }
     it 'should update everything from RGB sliders' do
